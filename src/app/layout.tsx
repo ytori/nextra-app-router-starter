f3c95b160f82f7fe40330e252292b35628e006ab
ytori <ysk.trge@gmail.com>
2024-10-08 19:17:31 +0900

    fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,35 @@
-import { Layout, Navbar } from "nextra-theme-docs";
-import { Banner, Head } from "nextra/components";
+import { Layout, Navbar, Footer } from "nextra-theme-docs";
+import { Head } from "nextra/components";
 import "./globals.css";
+import { Metadata } from "next";
+
+import { Inter } from "next/font/google";
+
+const siteConfig = {
+  name: "Nextra App Router Starter",
+  github: "https://github.com/ytori/nextra-app-router-starter",
+} as const;
 
 export const { viewport } = Head;
 
-export const metadata = {
-  metadataBase: new URL("https://nextra.site"),
+export const metadata: Metadata = {
   title: {
-    template: "%s - Nextra",
+    template: `%s - ${siteConfig.name}`,
+    default: siteConfig.name,
   },
-  description: "Nextra: the Next.js site builder",
-  applicationName: "Nextra",
+  description: "A starter kit for Nextra and Next.js App Router",
+  applicationName: `${siteConfig.name}`,
   generator: "Next.js",
   appleWebApp: {
-    title: "Nextra",
+    title: `${siteConfig.name}`,
   },
   other: {
     "msapplication-TileImage": "/ms-icon-144x144.png",
     "msapplication-TileColor": "#fff",
   },
-  twitter: {
-    site: "https://nextra.site",
-  },
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default async function RootLayout({
   children,
@@ -34,21 +41,37 @@ export default async function RootLayout({
   );
 
   return (
-    <html lang="en" dir="ltr" suppressHydrationWarning>
+    <html
+      lang="en"
+      dir="ltr"
+      className={inter.className}
+      suppressHydrationWarning
+    >
       <Head faviconGlyph="✦" />
       <body>
         <Layout
           editLink="Edit this page on GitHub"
-          docsRepositoryBase="https://github.com/shuding/nextra/blob/core/examples/docs"
+          docsRepositoryBase={`${siteConfig.github}/blob/main`}
           sidebar={{ defaultMenuCollapseLevel: 1 }}
           pageMap={pageMap}
         >
-          <Banner storageKey="Nextra 2">Nextra 2 Alpha</Banner>
+          {/*
+          <Banner storageKey="Nextra 4">Nextra 4.0.0-app-router</Banner>
+          */}
           <Navbar
-            // Next.js discord server
-            chatLink="https://discord.gg/hEM84NMkRv"
+            logo={
+              <span className="bg-gradient-to-r from-black to-gray-500 bg-clip-text text-xl font-bold tracking-tight text-transparent dark:from-white dark:to-gray-400">
+                {siteConfig.name}
+              </span>
+            }
+            projectLink={siteConfig.github}
           />
           {children}
+          <Footer>
+            <span className="text-sm font-semibold tracking-tight">
+              © {new Date().getFullYear()} - {siteConfig.name}
+            </span>
+          </Footer>
         </Layout>
       </body>
     </html>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "selector",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,12 +9,7 @@ const config: Config = {
     "./mdx/**/*.mdx",
   ],
   theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
